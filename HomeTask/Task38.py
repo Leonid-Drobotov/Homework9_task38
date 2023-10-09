122c85dc00cbe985444b22beb8660db64ae6c89b
Leonid Drobotov <fromarshincevo95@gmail.com>
2023-10-10 01:54:13 +0300

    added a main function for task

---
+++ HomeTask/Task38.py
@@ -0,0 +1,19 @@
+# Головная функция
+phonebook = dict()
+while True:
+        action = input(
+            "\nВыберите действие:\n1 - Добавить контакт\n2 - Изменить контакт\n3 - Удалить контакт\n4 - Вывести список контактов\n5 - Выйти из приложения\n")
+        if action == "1":
+            add_contact()
+        elif action == "2":
+            edit_contact()
+        elif action == "3":
+            delete_contact()
+        elif action == "4":
+            for name, data in phonebook.items():
+                print("{0}: {1}".format(name, data["номер"]))
+        elif action == "5":
+            print("Завершение работы")
+            break
+        else:
+            print("Некорректно введен пункт. Повторите попытку.")
